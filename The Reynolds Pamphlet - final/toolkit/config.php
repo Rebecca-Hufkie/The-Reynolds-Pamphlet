5ed455afa474c2921d32f19a217ac33f13615f83
Rebecca <100418094+Rebecca-Hufkie@users.noreply.github.com>
2024-10-21 19:33:08 +0200

    Update config.php

--- The Reynolds Pamphlet - final/toolkit/config.php
+++ The Reynolds Pamphlet - final/toolkit/config.php
@@ -1,5 +1,2 @@
 <?php
-    define ('SERVERNAME', 'cs3-dev.ict.ru.ac.za');
-    define('USERNAME', 'G22G8069');
-    define('PASSWORD', 'G22G8069');
-    define('DATABASE', 'G22G8069');
+   //removed for security
